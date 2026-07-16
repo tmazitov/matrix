3acb6c24933fdf7a992a4d397640b6cd9e241836
tmazitov <timurmazitov000@gmail.com>
2026-07-16 16:43:20 +0400

    update ex03: more examples for dot product func

--- ex03/cmd/main.go
+++ ex03/cmd/main.go
@@ -8,13 +8,25 @@ import (
 func main() {
 	u := vector.Vector[float64]{0.0, 0.0}
 	v := vector.Vector[float64]{1.0, 1.0}
-	fmt.Printf("Dot 1: %f\n", u.Dot(v))
+	fmt.Printf("Dot 1: %f\n", u.Dot(v)) // expected: 0.0
 
 	u = vector.Vector[float64]{1, 1}
 	v = vector.Vector[float64]{1, 1}
-	fmt.Printf("Dot 2: %f\n", u.Dot(v))
+	fmt.Printf("Dot 2: %f\n", u.Dot(v)) // expected: 2.0
 
 	u = vector.Vector[float64]{-1, 6}
 	v = vector.Vector[float64]{3, 2}
-	fmt.Printf("Dot 3: %f\n", u.Dot(v))
+	fmt.Printf("Dot 3: %f\n", u.Dot(v)) // expected: 9.0
+
+	u = vector.Vector[float64]{3, 2, 1}
+	v = vector.Vector[float64]{5, -2, 0}
+	fmt.Printf("Dot 4: %f\n", u.Dot(v)) // expected: 11.0
+
+	u = vector.Vector[float64]{0, -2, 5, 1}
+	v = vector.Vector[float64]{4, 1, -3, 2}
+	fmt.Printf("Dot 5: %f\n", u.Dot(v)) // expected: -15.0
+
+	u = vector.Vector[float64]{2.5, -1.5}
+	v = vector.Vector[float64]{2.5, -1.5}
+	fmt.Printf("Dot 6: %f\n", u.Dot(v)) // expected: 8.5
 }
